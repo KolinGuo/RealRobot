48ca498db4bba910b268931974ff3be6f9c56bd8
Kolin Guo <ruguo@ucsd.edu>
2024-06-15 23:49:48 -0700

    Make xarm as an optional dependency (installed via real_robot[xarm])

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="real_robot",
-        package_dir={
-            "real_robot": "real_robot",
-            "xarm": "3rd_party/xArm-Python-SDK/xarm",
-        },
+        package_dir={"real_robot": "real_robot"},
         package_data={"real_robot": ["assets/**"]},
         exclude_package_data={"": ["*.convex.stl"]},
+        extras_require={
+            "xarm": [
+                "xArm-Python-SDK @ git+https://github.com/KolinGuo/xArm-Python-SDK.git@custom"
+            ]
+        },
         zip_safe=False,
     )
